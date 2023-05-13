400665b3f52e9838c6971f71bdf52b7d9d64c9ee
2A42 <sizov.arteom2015@yandex.ru>
2023-05-13 21:51:40 +0500

    Revert "Revert "Added new functions""
    This reverts commit 0366635aebe65774cad82dcc509f20b4c5140b17.

---
+++ source/api.py
@@ -0,0 +1,76 @@
+import requests
+import shutil
+import json
+
+from config import AI_API_KEY #custom info
+
+def get_joke():
+    limit = 1
+    api_url = 'https://api.api-ninjas.com/v1/jokes?limit={}'.format(limit)
+    response = requests.get(api_url, headers={'X-Api-Key': AI_API_KEY})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        for p in data:
+            return p['joke']
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
+
+def get_dad_joke():
+    limit = 1
+    api_url = 'https://api.api-ninjas.com/v1/dadjokes?limit={}'.format(limit)
+    response = requests.get(api_url, headers={'X-Api-Key': AI_API_KEY})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        for p in data:
+            return p['joke']
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
+
+def get_rword():
+    api_url = 'https://api.api-ninjas.com/v1/randomword'
+    response = requests.get(api_url, headers={'X-Api-Key': AI_API_KEY})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        return data['word']
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
+
+def get_definition(word):
+    api_url = 'https://api.api-ninjas.com/v1/dictionary?word={}'.format(word)
+    response = requests.get(api_url, headers={'X-Api-Key': AI_API_KEY})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        if data['valid'] == True:
+            return word + ' - ' + data['definition']
+        else:
+            return None
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
+
+def get_image():
+    api_url = 'https://api.api-ninjas.com/v1/randomimage'
+    response = requests.get(api_url, headers={'X-Api-Key': AI_API_KEY, 'Accept': 'image/jpg'}, stream=True)
+    if response.status_code == requests.codes.ok:
+        with open('img.jpg', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
+
+def get_image_info(image):
+    api_url = 'https://api.api-ninjas.com/v1/objectdetection'
+    files = {'image': image}
+    response = requests.post(api_url, headers={'X-Api-Key': AI_API_KEY}, files=files)
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        result = ""
+        for p in data:
+            result = result + "It's " + p['label'] + " with " + str(int(float(p['confidence']) * 100)) + "% of confidence\n"
+        return result
+    else:
+        error = "Error:" + str(response.status_code) + str(response.text)
+        return error
